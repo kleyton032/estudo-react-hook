59391aba06224183de1a9ba1554df40ca741acd5
Kleyton João <kleyton.joao@gmail.com>
2021-07-31 10:24:01 -0300

    useContext + useReducer

--- src/App.js
+++ src/App.js
@@ -1,87 +1,60 @@
-
-// const Button = React.memo(({incrementButton}) => {
-//   console.log("filho renderizou")
-//   return <button onClick={()=> incrementButton(10)}>+</button>
-// });
-
-import { useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
+import P from 'prop-types';
 
 
-const globaState = {
+export const actions = {
+  CANGHE_TITLE: 'CANGHE_TITLE',
+}
+export const globaState = {
   title: 'O título do texto',
   body: 'O conteudo do corpo',
   counter: 0
 }
 
-const reducer = (state, action) => {
+export const reducer = (state, action) => {
   console.log(action)
-  switch(action.type){
-    case 'muda': {
-      console.log('chamou o muda', action.payload);
-      return {...state, title: action.payload}
+  switch (action.type){
+    case actions.CANGHE_TITLE: {
+      console.log("Muda o título")
+      return {...state, title: "Mudou o título"}
     }
-    case 'inverte': {
-      console.log('chamou o inverte')
-      const {title} = state;
-      return {...state, title: title.split('').reverse().join('')}
-    }
-      
   }
-  return {...state};
+  return { ...state };
+}
+
+export const Context = createContext();
+export const AppContext = ({ children }) => {
+
+  const changeTitle = () => {
+    dispatch({type: actions.CANGHE_TITLE})
+  }
+
+  const [state, dispatch] = useReducer(reducer, globaState);
+  return (
+    <Context.Provider value={{ state, changeTitle }}>
+      {children}
+    </Context.Provider>
+  )
+}
+
+AppContext.propTypes = {
+  children: P.node,
+}
+
+export const H1 = () => {
+  const context = useContext(Context);
+  return <h1 onClick={()=> context.changeTitle()}>{context.state.title}</h1>
 }
 
 function App() {
-//estudo useReducer
-const [state, dispatch] = useReducer(reducer, globaState);
-const {title, body, counter} = state;
+  //estudo useReducer + useContext
   return (
-    <div className="App">
-      <h1>{title}</h1>
-      <h1>{counter}</h1>
-      <button onClick={()=> dispatch({type:'muda', payload: new Date().toLocaleDateString('pt-BR')})}>Clique</button>
-      <button onClick={()=> dispatch({type:'inverte'})}>INVERTE</button>
-    </div>      
+    <AppContext>
+      <div className="App">
+        <H1 />
+      </div>
+    </AppContext>
   );
-  
-//   //const [reverse, setReverse] = useState(false);
-//   const [counter, setCounter] = useState(0)
-//   //const reverseClass = reverse ? 'reverse' : '';
-
-//   const incrementCounter = useCallback((num) => {
-//     setCounter((c) => c + num);
-//   }, []);
-
-//   console.log("pai renderizou")
-//   //componentDidUpdate - executa toda vez que o component atualiza
-//   // useEffect(() => {
-//   //   console.log("componentDidUpdate");
-//   // });
-
-//   // //componentDidMount - executa 1x
-//   // useEffect(() => {
-//   //   console.log("componentDidMount");
-//   // },[]);
-
-//   // useEffect(() => {
-//   //   console.log(`componentDidMount com dependências ${counter}`);
-//   // },[counter]);
-
-//   return (
-//     <div className="App">
-//      <h1>Contador: {counter}</h1>
-//      <Button incrementButton={incrementCounter}/>
-//     </div>
-//   );
-// }
-
-{/* {posts.map(post => {
-        return (
-          <div key={post.id} className="post">
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        )
-      })} */}
 }
 
 export default App;
