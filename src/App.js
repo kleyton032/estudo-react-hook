5bd5716e17584a619ab03871eb339fb22cc95dd8
Kleyton João <kleyton.joao@gmail.com>
2021-10-01 09:14:04 -0300

    custom my hook

--- src/App.js
+++ src/App.js
@@ -1,60 +1,52 @@
 import { createContext, useContext, useReducer } from "react";
 import P from 'prop-types';
 
+const useMyHook = (cb, delay = 1000) => {
 
-export const actions = {
-  CANGHE_TITLE: 'CANGHE_TITLE',
-}
-export const globaState = {
-  title: 'O título do texto',
-  body: 'O conteudo do corpo',
-  counter: 0
-}
+  const savedCb = useRef();
 
-export const reducer = (state, action) => {
-  console.log(action)
-  switch (action.type){
-    case actions.CANGHE_TITLE: {
-      console.log("Muda o título")
-      return {...state, title: "Mudou o título"}
-    }
-  }
-  return { ...state };
-}
+  useEffect(() => {
+    savedCb.current = cb;
+  }, [cb])
 
-export const Context = createContext();
-export const AppContext = ({ children }) => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      cb();
+    }, delay);
 
-  const changeTitle = () => {
-    dispatch({type: actions.CANGHE_TITLE})
-  }
+    return () => clearInterval(interval);
+  }, [delay]);
+};
 
-  const [state, dispatch] = useReducer(reducer, globaState);
+function App() {
+
+  const [counter, setCounter] = useState(0);
+  const [delay, setDelay] = useState(1000);
+  const [incrementor, setIncrementor] = useState(100);
+
+  useMyHook(() => setCounter((c) => c + 1), delay);
+
   return (
-    <Context.Provider value={{ state, changeTitle }}>
-      {children}
-    </Context.Provider>
+    <div>
+      <h1>Contador: {counter}</h1>
+      <h1>Delay: {delay}</h1>
+      <button onClick={() => {
+        setDelay((d) => d + incrementor)
+      }}>
+        + {incrementor}
+      </button>
+
+
+      <button onClick={() => {
+        setDelay((d) => d - incrementor)
+      }}>
+
+        - {incrementor}
+      </button>
+      <input type="number" value={incrementor} onChange={(e) => setIncrementor(e.target.value)} />
+    </div>
   )
 }
 
-AppContext.propTypes = {
-  children: P.node,
-}
-
-export const H1 = () => {
-  const context = useContext(Context);
-  return <h1 onClick={()=> context.changeTitle()}>{context.state.title}</h1>
-}
-
-function App() {
-  //estudo useReducer + useContext
-  return (
-    <AppContext>
-      <div className="App">
-        <H1 />
-      </div>
-    </AppContext>
-  );
-}
 
 export default App;
